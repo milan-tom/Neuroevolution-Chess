2758ff6ba286d3b1c821a99209a0dc3f14959f2b
milan-tom <milant076@gmail.com>
2021-06-17 18:31:48 +0100

    Integrate bitboard into overarching chess class

--- core_chess.py
+++ core_chess.py
@@ -1,14 +1,20 @@
-class Bitboard:
-    def __init__(self, fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"):
-        self.fen_to_bitboard(fen)
+class Chess:
+    def __init__(self, fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
+        (
+            positions,
+            self.next_colour,
+            self.castling,
+            self.en_passant_square,
+            self.half_move_clock,
+            self.move_number,
+        ) = fen.split()
 
-    def fen_to_bitboard(self, fen):
         self.board = {piece: int() for piece in "KQRBNPkqrbnp"}
-        for row_i, row in enumerate(reversed(fen.split("/"))):
+        for row_i, row in enumerate(reversed(positions.split("/"))):
             piece_i = 0
             for piece in reversed(row):
                 if piece.isnumeric():
                     piece_i += int(piece)
                 else:
-                    self.board[piece] += 2 ** (row_i * 8 + piece_i)
+                    self.board[piece] += 1 << (row_i * 8 + piece_i)
                     piece_i += 1
