e2551b739c1640231167400283f1f4e0a1ff727e
Milan Tom <48379919+milan-tom@users.noreply.github.com>
2021-08-06 20:03:51 +0100

    Update GUI.py

--- chess_gui/GUI.py
+++ chess_gui/GUI.py
@@ -13,18 +13,24 @@ class ChessGUI:
         self.draw_board()
 
     def draw_board(self):
+        """Displays the current state of the board"""
+        # Initialises list of rectangle (i.e. board square) coordinates
         rectangle = [0, 0, self.square_size, self.square_size]
+        # Loops through each row and column, drawing squares and pieces
         for row in range(8):
+            # Updates coordinates and square colours as appropriate
             rectangle[1] = row * self.square_size
             for column in range(8):
                 rectangle[0] = column * self.square_size
+                # Draws chess board square
                 pygame.draw.rect(
                     self.surface, self.board_colours[(row + column) % 2], rectangle
                 )
-
+        # Updates display to render changes
         pygame.display.update()
 
     def mainloop(self):
+        """Keeps GUI running, handling events and rendering changes"""
         running = True
         while running:
             for event in pygame.event.get():
