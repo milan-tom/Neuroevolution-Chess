77b5c81321067d5b03844ccc16b77136f834daa5
milan-tom <milant076@gmail.com>
2021-11-05 19:06:14 +0000

    Remove creation of dummy driver needed for POSIX systems

--- chess_gui/GUI.py
+++ chess_gui/GUI.py
@@ -8,9 +8,6 @@ from pygame_widgets.button import Button
 
 from core_chess.chess_logic import Chess, STARTING_FEN
 
-# Adds dummy video driver for machines without displays (e.g. testing from Linux server)
-if os.name == "posix" and "DISPLAY" not in os.environ:
-    os.environ["SDL_VIDEODRIVER"] = "dummy"
 # Instructs OS to open window slightly offset so all of it fits on the screen
 os.environ["SDL_VIDEO_WINDOW_POS"] = "0, 20"
 
